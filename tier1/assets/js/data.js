4ac9f442bfea42d8f8aaa9a42d993706527ed67c
homecareboost <services@homecareboost.com>
2024-12-11 16:13:12 -0500

    initial file copy

---
+++ tier1/assets/js/data.js
@@ -0,0 +1,133 @@
+const data = {
+    //CLIENT:TEMPLATE
+    // MANUALLY INSERT SURGE INFO HERE
+    // keyword: "KEYWORD. Senior Helpers Indianapolis Home Care",
+    pagetitle: "XXX - INSERT BRAND + CITY + HOME CARE.", 
+    targeturl: "XXX INSERT SURGE TARGET URL",
+    metadescription: "XXX Just change the name in this description to client name> Senior Helpers is a home care agency comitted to providing aged care and home health care to the elderly living in the XXX CITY, ",
+    aboutus: "XXX INSERT CHAT GPT DESRIPTION HERE",
+    youtubeurl: "XXX INSERT SURGE YOUTUBE URL LIKE: https://www.youtube.com/watch?v=2w7OI6BGP7s",
+    youtubevideoid: "XXX INSERT SURGE VIDEO ID LIKE 2w7OI6BGP7s",
+    youtubechannelurl: "XXX INSERT SURGE CHANNEL URL LIKE: https://www.youtube.com/@SeniorHelpersIndianapolis",
+    youtubeembed: "XXX INSERT SURGE YOUTUBE EMBED LIKE <iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/2w7OI6BGP7s?si=l9NgmYZh0LpSIkB6\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" referrerpolicy=\"strict-origin-when-cross-origin\" allowfullscreen></iframe>",
+    rssfeedembed: "XXX INSERT SURGE RSS FEED https://rss.app/feeds/sZq8gk5UWvrrwbJC.xml",
+    surgeimage: "XXX",
+    youtuberelatedvideourl1: "XXX",
+    youtuberelatedvideourl2: "XXX",
+    articletitle: "Senior Helpers Indianapolis Article Title", 
+    ownerwebsiteurl: "https://www.seniorhelpers.com/in/indianapolis/meet-the-office/",
+    soundcloud: "XXX INSERT SOUNDCLOUD URL LIKE https://soundcloud.com/seniorhelpers-indiana",
+    googlefolder: "XXX INSERT SURGE PUBLIC GOOGLE DRIVE FOLDER like https://drive.google.com/drive/folders/1byUcU6bhfjUZAQ7EHF88f4_0AtY3oLe6?usp=drive_link",
+    spotify: "XXX INSERT SPOTIFY URL LIKE https://podcasters.spotify.com/pod/show/senior-helpers-indianapol",
+    wiki: "XXX",
+
+  
+    // AUTO INSERTED INFO FROM CLARA
+    businessname: "BUSINESS NAME",
+    fulladdress: "123 Main Street Anytown, State 12313",
+    street: "123 Main Street",
+    city: "Anytown",
+    state: "STATE",
+    zip: "46254",
+    latLong: "39.82603990, -86.28175260",
+    gmbphone: "(111) 111 1111",
+    gmbmapembed: "",
+    gmbimageurl: "https://lh3.googleusercontent.com/p/AF1QipMGjJr2rLEadzr2qsFtGhgmm4QBU0i3YN-aTTcA=s680-w680-h510",
+    gmblongurl: "https://www.google.it/maps/place/Senior+Helpers/@39.826044,-86.2843275,17z/data=!3m1!4b1!4m6!3m5!1s0x886ca993d82f82cf:0xd112964f24b6ced6!8m2!3d39.8260399!4d-86.2817526!16s%2Fg%2F1q67gw6dw?entry=ttu&g_ep=EgoyMDI0MDkxMS4wIKXMDSoASAFQAw%3D%3D",
+    websiteurl: "www.website.com",
+    facebook: "https://www.facebook.com/",
+    twitter: "https://x.com",
+    linkedin: "",
+    yelp: "https://www.yelp.com/biz",
+    wikidataurl: "",
+    crunchbasecompanyurl: "https://www.crunchbase.com",
+    yelp: "https://www.yelp.com/biz",
+    logofilename: "https://www.seniorhelpers.com/site/templates//images/sh-footer-logo.png",
+    publicemail: "",
+    ownername: "FIRST LAST",
+    descriptionshort: "FAKE COMPANY NAME provides professional and compassionate in-home care for seniors. Our caregivers assist with daily activities, Alzheimer’s and dementia care, and personalized services to improve quality of life. Trust us to support your loved ones with expert care and peace of mind. Serving Indianapolis and surrounding areas. ",
+    descriptionmedium: "FAKE COMPANY NAME offers exceptional in-home care tailored to meet the unique needs of seniors. Our trained caregivers provide a wide range of services, including assistance with daily activities, specialized Alzheimer's and dementia care, and respite care for family caregivers. We are dedicated to improving the quality of life for our clients by promoting independence and offering compassionate support. Whether your loved one needs short-term or long-term care, our team ensures they receive personalized, professional assistance. Serving seniors in Indianapolis and nearby communities, we strive to bring peace of mind to families.",
+    descriptionmedium: "FAKE COMPANY NAME offers exceptional in-home care tailored to meet the unique needs of seniors. Our trained caregivers provide a wide range of services, including assistance with daily activities, specialized Alzheimer's and dementia care, and respite care for family caregivers. We are dedicated to improving the quality of life for our clients by promoting independence and offering compassionate support. Whether your loved one needs short-term or long-term care, our team ensures they receive personalized, professional assistance. Serving seniors in Indianapolis and nearby communities, we strive to bring peace of mind to families.",
+    pagecaregivers: "https://www.website.com/in/indianapolis/resources/caregivers-for-seniors/",
+    page24hour: "https://www.website.com/in/indianapolis/services/24-hour-home-care/",
+    pagedementia: "https://www.website.com/in/indianapolis/services/alzheimers-dementia-care/",
+    pagehomecare: "https://www.website.com/in/indianapolis/resources/home-care-indianapolis/",
+    pagehospice: "",
+    pageprivate: "",
+    pagerespite: "https://www.website.com/in/indianapolis/services/respite-care/",    
+    town1name: "TOWN1",
+    town1url: "",
+    town1state: "STATE",
+    town1wikiurl: "https://www.wikidata.org/wiki/Q7802085",
+    town2name: "TOWN2",
+    town2url: "",
+    town2state: "STATE",
+    town2wikiurl: "",
+    town3name: "TOWN3",
+    town3url: "",
+    town3state: "STATE",
+    town3wikiurl: "https://www.wikidata.org/wiki/Q791269",
+    town4name: "TOWN4",
+    town4url: "",
+    town4state: "STATE",
+    town4wikiurl: "https://en.wikipedia.org/wiki/Brownsburg,_Indiana",
+    town5name: "TOWN5",
+    town5url: "",
+    town5state: "STATE",
+    town5wikiurl: "https://en.wikipedia.org/wiki/Carmel,_Indiana",
+    town6name: "Meridian Hills",
+    town6url: "TOWN6",
+    town6state: "STATE",
+    town6wikiurl: "https://en.wikipedia.org/wiki/Meridian_Hills,_Indiana",
+    town7name: "TOWN7",
+    town7url: "",
+    town7state: "STATE",
+    town7wikiurl: "https://en.wikipedia.org/wiki/Clermont,_Indiana",
+    town8name: "TOWN8",
+    town8url: "",
+    town8state: "STATE",
+    town8wikiurl: "https://en.wikipedia.org/wiki/Zionsville,_Indiana",
+    town9name: "TOWN9",
+    town9url: "",
+    town9state: "",
+    town9wikiurl: "",
+    town10name: "TOWN10",
+    town10url: "",
+    town10state: "",
+    town10wikiurl: "",
+
+  header() {
+    return `
+      <ul>
+        <li><a href="index.html" class="active">Home</a></li>
+        <li class="dropdown"><a href="#"><span>Services</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+            <ul>
+                <li><a href="home-care.html">Home Care</a></li>
+                <li><a href="24-hour-home-care.html">24 Hour Home Care</a></li>
+                <li><a href="caregivers-for-seniors.html">Caregivers for Seniors</a></li>
+                <li><a href="hospice-care-at-home.html">Hospice care at home</a></li>
+                <li><a href="alzheimers-and-dementia-care.html">Alzheimers and Dementia Care</a></li>
+                <li><a href="private-home-care-aide.html">Private Home Care Aide</a></li>
+                <li><a href="respite-care.html">Respite Care</a></li>
+            </ul>
+        </li>
+        <li><a href="contactus.html">Contact & About Us</a></li>
+    </ul>`;
+  },
+  footer() {
+    return `
+      <footer id="footer" class="footer light-background">
+        <div class="container">
+        <div class="copyright text-center ">
+            <p>© ${new Date().getFullYear()} <span>Copyright</span> <strong class="px-1 sitename">${this.businessname}</strong> <span>All Rights Reserved</span></p>
+        </div>
+          <!-- <div class="social-links d-flex justify-content-center">
+                <a href=""><i class="bi bi-twitter-x"></i></a>
+                <a href=""><i class="bi bi-facebook"></i></a>
+                <a href=""><i class="bi bi-instagram"></i></a>
+                <a href=""><i class="bi bi-linkedin"></i></a>
+            </div> -->
+        </div>
+      </footer>`;
+  }
+}
---
+++ tier2/cloudflare_to_do/assets/js/data.js
@@ -0,0 +1,132 @@
+const data = {
+    // MANUALLY INSERT SURGE INFO HERE
+    //LINODE TO https://seniorhelpersindianapoli.blob.core.windows.net/seniorhelpersindianapolis/index.html
+    //BUNNY TO https://seniorhelpersindianapolis.nyc3.digitaloceanspaces.com/index.html
+    //CLOUDFLARE TO https://seniorhelpers-indianapolis.s3.us-east-2.amazonaws.com/index.html
+    //
+    //
+    keyword: "Senior Helpers Indianapolis Home Care",
+    pagetitle: "Senior Helpers Indianapolis Home Care",
+    targeturl: "https://seniorhelpersindianapolis.nyc3.digitaloceanspaces.com/index.html",
+    metadescription: "Senior Helpers is a home care agency comitted to providing aged care and home health care to the elderly living in the Indianapolis, ",
+    aboutus: "Senior Helpers Indianapolis: Providing Professional and Compassionate In-Home Care for Seniors in Indianapolis, Indiana Located in the heart of Indianapolis, Indiana, Senior Helpers Indianapolis has become a trusted name in in-home care, offering personalized and compassionate support to seniors and their families for over a decade. With a strong commitment to enhancing the quality of life for seniors, Senior Helpers focuses on creating safe, comfortable, and independent living environments for their clients, while providing peace of mind for their families.  Senior Helpers Indianapolis offers an extensive range of services designed to assist seniors with daily living activities and provide specialized care for those with more complex health needs. Our caregivers are expertly trained to support seniors with personal care tasks such as bathing, grooming, meal preparation, medication reminders, and light housekeeping. Whether your loved one needs assistance with mobility, companionship during social outings, or specialized Alzheimer’s and dementia care, Senior Helpers tailors its services to meet the unique needs of every individual.  What sets Senior Helpers Indianapolis apart is our person-centered approach to care. We understand that every senior has different needs, preferences, and routines, which is why we work closely with families to develop customized care plans that focus on the well-being of the individual. Whether your loved one requires part-time assistance, full-time support, or 24-hour live-in care, our caregivers are dedicated to providing compassionate and respectful service that prioritizes comfort, dignity, and independence.  One of our core specialties is Alzheimer’s and dementia care. Senior Helpers Indianapolis has developed a specialized care program that caters to seniors living with these cognitive challenges. Our caregivers receive advanced training to offer meaningful interactions, memory stimulation, and emotional support, helping seniors maintain cognitive function while also reducing stress for both the individual and their families. We incorporate activities that are tailored to the individual’s cognitive abilities, promoting engagement and improving the overall quality of life.  In addition to our specialized care programs, Senior Helpers Indianapolis offers respite care for family caregivers. We understand that caring for a loved one can be emotionally and physically exhausting, which is why we provide temporary relief, allowing family members to take a break from caregiving responsibilities while knowing their loved one is in capable hands. Our flexible care services range from a few hours of assistance to longer-term solutions, ensuring that caregivers can rest and recharge without compromising the quality of care their loved ones receive.  Our commitment to communication and transparency is another key feature of our services. At Senior Helpers Indianapolis, we believe in maintaining open lines of communication with families to keep them informed and involved in their loved one’s care. We regularly update families on the progress and well-being of their loved ones, offering peace of mind and fostering trust.  Beyond just physical care, we emphasize the importance of emotional well-being. Our caregivers are trained to provide companionship, engaging seniors in meaningful conversations and activities that bring joy and fulfillment. For seniors living with dementia or Alzheimer's, our caregivers offer specialized cognitive stimulation activities, designed to keep their minds active and their spirits high.  Serving the greater Indianapolis area, we are proud to be a part of the community and to offer local families reliable, high-quality in-home care. Whether your loved one requires temporary care following an illness or surgery, or long-term support to age gracefully at home, Senior Helpers Indianapolis is here to help. With over a decade of experience in home care, we are dedicated to delivering exceptional service and ensuring that every client receives the individualized care they deserve.  Senior Helpers Indianapolis is more than just a caregiving service – we are partners in care. Our compassionate and skilled team is committed to helping seniors maintain their independence, dignity, and quality of life, allowing them to age safely and comfortably in the familiar surroundings of their own homes.",
+    youtubeurl: "https://www.youtube.com/watch?v=2w7OI6BGP7s",
+    youtubevideoid: "2w7OI6BGP7s",
+    youtubechannelurl: "https://www.youtube.com/@SeniorHelpersIndianapolis",
+    youtubeembed: "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/2w7OI6BGP7s?si=l9NgmYZh0LpSIkB6\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" referrerpolicy=\"strict-origin-when-cross-origin\" allowfullscreen></iframe>",
+    rssfeedembed: "https://rss.app/feeds/sZq8gk5UWvrrwbJC.xml",
+    surgeimage: "XXX",
+    youtuberelatedvideourl1: "XXX",
+    youtuberelatedvideourl2: "XXX",
+    articletitle: "Senior Helpers Indianapolis Article Title", 
+    ownerwebsiteurl: "https://www.seniorhelpers.com/in/indianapolis/meet-the-office/",
+  
+    // AUTO INSERTED INFO FROM CLARA
+    businessname: "Senior Helpers",
+    fulladdress: "6845 Parkdale Place, Suite C Indianapolis, IN 46254",
+    street: "6845 Parkdale Place, Suite C",
+    city: "Indianapolis",
+    state: "IN",
+    zip: "46254",
+    latLong: "39.82603990, -86.28175260",
+    gmbphone: "(317) 927-7700",
+    gmbmapembed: "",
+    gmbimageurl: "https://lh3.googleusercontent.com/p/AF1QipPNffSRkQqCwktB7Fjepk4wZx8dCuKQaILJ1eEy=s680-w680-h510",
+    gmblongurl: "https://www.google.it/maps/place/Senior+Helpers/@39.826044,-86.2843275,17z/data=!3m1!4b1!4m6!3m5!1s0x886ca993d82f82cf:0xd112964f24b6ced6!8m2!3d39.8260399!4d-86.2817526!16s%2Fg%2F1q67gw6dw?entry=ttu&g_ep=EgoyMDI0MDkxMS4wIKXMDSoASAFQAw%3D%3D",
+    websiteurl: "www.seniorhelpers.com/in/indianapolis/",
+    facebook: "https://www.facebook.com/SeniorHelpersIndy/",
+    twitter: "https://x.com/seniorhelpindy",
+    linkedin: "",
+    yelp: "https://www.yelp.com/biz/senior-helpers-indianapolis",
+    wikidataurl: "",
+    crunchbasecompanyurl: "https://www.crunchbase.com/organization/senior-helpers-indianapolis",
+    yelp: "https://www.yelp.com/biz/senior-helpers-indianapolis",
+    logofilename: "https://www.seniorhelpers.com/site/templates//images/sh-footer-logo.png",
+    publicemail: "",
+    ownername: "Brian Jones",
+    descriptionshort: "Senior Helpers Indianapolis provides professional and compassionate in-home care for seniors. Our caregivers assist with daily activities, Alzheimer’s and dementia care, and personalized services to improve quality of life. Trust us to support your loved ones with expert care and peace of mind. Serving Indianapolis and surrounding areas. ",
+    descriptionmedium: "Senior Helpers Indianapolis offers exceptional in-home care tailored to meet the unique needs of seniors. Our trained caregivers provide a wide range of services, including assistance with daily activities, specialized Alzheimer's and dementia care, and respite care for family caregivers. We are dedicated to improving the quality of life for our clients by promoting independence and offering compassionate support. Whether your loved one needs short-term or long-term care, our team ensures they receive personalized, professional assistance. Serving seniors in Indianapolis and nearby communities, we strive to bring peace of mind to families.",
+    descriptionmedium: "Senior Helpers Indianapolis offers exceptional in-home care tailored to meet the unique needs of seniors. Our trained caregivers provide a wide range of services, including assistance with daily activities, specialized Alzheimer's and dementia care, and respite care for family caregivers. We are dedicated to improving the quality of life for our clients by promoting independence and offering compassionate support. Whether your loved one needs short-term or long-term care, our team ensures they receive personalized, professional assistance. Serving seniors in Indianapolis and nearby communities, we strive to bring peace of mind to families.",
+    pagecaregivers: "https://www.seniorhelpers.com/in/indianapolis/resources/caregivers-for-seniors/",
+    page24hour: "https://www.seniorhelpers.com/in/indianapolis/services/24-hour-home-care/",
+    pagedementia: "https://www.seniorhelpers.com/in/indianapolis/services/alzheimers-dementia-care/",
+    pagehomecare: "https://www.seniorhelpers.com/in/indianapolis/resources/home-care-indianapolis/",
+    pagehospice: "",
+    pageprivate: "",
+    pagerespite: "https://www.seniorhelpers.com/in/indianapolis/services/respite-care/",    
+    town1name: "Tilden",
+    town1url: "",
+    town1state: "IN",
+    town1wikiurl: "https://www.wikidata.org/wiki/Q7802085",
+    town2name: "Eagle Creek ",
+    town2url: "",
+    town2state: "IN",
+    town2wikiurl: "",
+    town3name: "Avon",
+    town3url: "",
+    town3state: "IN",
+    town3wikiurl: "https://www.wikidata.org/wiki/Q791269",
+    town4name: "Brownsburg",
+    town4url: "",
+    town4state: "IN",
+    town4wikiurl: "https://en.wikipedia.org/wiki/Brownsburg,_Indiana",
+    town5name: "Carmel",
+    town5url: "",
+    town5state: "IN",
+    town5wikiurl: "https://en.wikipedia.org/wiki/Carmel,_Indiana",
+    town6name: "Meridian Hills",
+    town6url: "",
+    town6state: "IN",
+    town6wikiurl: "https://en.wikipedia.org/wiki/Meridian_Hills,_Indiana",
+    town7name: "Clermont",
+    town7url: "",
+    town7state: "IN",
+    town7wikiurl: "https://en.wikipedia.org/wiki/Clermont,_Indiana",
+    town8name: "Zionsville",
+    town8url: "",
+    town8state: "IN",
+    town8wikiurl: "https://en.wikipedia.org/wiki/Zionsville,_Indiana",
+    town9name: "",
+    town9url: "",
+    town9state: "",
+    town9wikiurl: "",
+    town10name: "",
+    town10url: "",
+    town10state: "",
+    town10wikiurl: "",
+
+  header() {
+    // return `
+    //   <ul>
+    //     <li><a href="index.html" class="active">Home</a></li>
+    //     <li class="dropdown"><a href="#"><span>Services</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+    //         <ul>
+    //             <li><a href="home-care.html">Home Care</a></li>
+    //             <li><a href="24-hour-home-care.html">Respite Home Care</a></li>
+    //             <li><a href="caregivers-for-seniors.html">Caregivers for Seniors</a></li>
+    //             <li><a href="hospice-care-at-home.html">Hospice care at home</a></li>
+    //             <li><a href="alzheimers-and-dementia-care.html">Alzheimers and Dementia Care</a></li>
+    //             <li><a href="private-home-care-aide.html">Private Home Care Aide</a></li>
+    //             <li><a href="#">Respite Care</a></li>
+    //         </ul>
+    //     </li>
+    //     <li><a href="contactus.html">Contact & About Us</a></li>
+    // </ul>`;
+  },
+  footer() {
+    return `
+      <footer id="footer" class="footer light-background">
+        <div class="container">
+        <div class="copyright text-center ">
+            <p>© ${new Date().getFullYear()} <span>Copyright</span> <strong class="px-1 sitename">${this.businessname}</strong> <span>All Rights Reserved</span></p>
+        </div>
+          <!-- <div class="social-links d-flex justify-content-center">
+                <a href=""><i class="bi bi-twitter-x"></i></a>
+                <a href=""><i class="bi bi-facebook"></i></a>
+                <a href=""><i class="bi bi-instagram"></i></a>
+                <a href=""><i class="bi bi-linkedin"></i></a>
+            </div> -->
+        </div>
+      </footer>`;
+  }
+}
